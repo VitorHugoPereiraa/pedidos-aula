0bd85f603f2d9ab4fa16e5f25d47ab10538182a6
Vitor Hugo Pereira <vitorhugopereiraa@outlook.com>
2023-10-24 22:14:56 -0300

    updates

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,43 +1,40 @@
 import React, { useEffect, useState } from "react";
-import { ActivityIndicator, Text, View } from "react-native";
 import api from "../../services/api";
-import ItemDePedido from "../../components/ItemDePedido";
+import { ScrollView, Text, View } from "react-native";
 
 const Home = (props) => {
-  const [pedidos, setPedidos] = useState([]);
-  const [loading, setLoading] = useState(true);
-
+  const [socios, setSocios] = useState([]);
+  const [sociosFX7, setSociosFX7] = useState([]);
+  const [sociosFX6, setSociosFX6] = useState([]);
   useEffect(() => {
-    setTimeout(() => {
-      (async () => {
-        let res = await api.pegarPedidos();
-        setPedidos(res);
-        setLoading(false);
-      })();
-    }, 2000);
+    // (async function () {})();
+    // (async () => {})();
+    (async () => {
+      let data = await api.getByCNPJ("61186888009301");
+      let socios_ = data.data.qsa;
+      setSocios(socios_);
+      //filtrar os socios que possui codigo_faixa_etaria igual a 7
+      setSociosFX7(socios_.filter((socio) => socio.codigo_faixa_etaria === 7));
+
+      //filtrar os socios que possui codigo_faixa_etaria igual a 5
+      setSociosFX6(socios_.filter((socio) => socio.codigo_faixa_etaria === 5));
+    })();
   }, []);
-
   return (
-    <View style={{ flex: 1, padding: 20 }}>
-      {loading ? (
-        <ActivityIndicator />
-      ) : (
-        <View>
-          {pedidos.map((pedido) => {
-            return <ItemDePedido pedido={pedido} />;
-          })}
-        </View>
-      )}
-
-      {/* {loading && <ActivityIndicator />}
-      {!loading && (
-        <View>
-          {pedidos.map((pedido) => (
-            <Text key={pedido.id}>{pedido.client}</Text>
-          ))}
-        </View>
-      )} */}
-    </View>
+    <ScrollView style={{ flex: 1, padding: 20 }}>
+      <Text style={{ marginVertical: 20 }}>
+        TODOS OS SOCIOS COM FAIXA ETARIA 7 - QTY {sociosFX7.length}:
+      </Text>
+      {sociosFX7.map((socio) => {
+        return <Text>{socio.nome_socio}</Text>;
+      })}
+      <Text style={{ marginVertical: 20 }}>
+        TODOS OS SOCIOS COM FAIXA ETARIA 5 - QTY {sociosFX6.length}:
+      </Text>
+      {sociosFX6.map((socio) => (
+        <Text>{socio.nome_socio}</Text>
+      ))}
+    </ScrollView>
   );
 };
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,32 +1,7 @@
-//status 0 === novo
-//status 1 === em andamento
-//status 2 === finalizado
-let pedidos = [
-  {
-    id: 1,
-    client: "Cristiano",
-    date: "2023-11-20",
-    products: [],
-    status: 0,
-  },
-  {
-    id: 2,
-    client: "Vitor",
-    date: "2023-09-20",
-    products: [],
-    status: 1,
-  },
-  {
-    id: 3,
-    client: "Lucas",
-    date: "2023-12-20",
-    products: [],
-    status: 2,
-  },
-];
+import api from "./axios";
 
 export default {
-  pegarPedidos: async () => {
-    return pedidos;
+  getByCNPJ: async (cnpj = "") => {
+    return api.get(cnpj);
   },
 };

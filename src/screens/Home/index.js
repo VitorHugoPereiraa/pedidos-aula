e44a3a31e39972b402a8a1d6a89dc6542f281073
Vitor Hugo Pereira <vitorhugopereiraa@outlook.com>
2023-11-07 21:57:06 -0300

    update

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -3,39 +3,69 @@ import api from "../../services/api";
 import { ScrollView, Text, View } from "react-native";
 
 const Home = (props) => {
+  const [estaPronto, setEstaPronto] = useState(0);
   const [socios, setSocios] = useState([]);
   const [sociosFX7, setSociosFX7] = useState([]);
   const [sociosFX6, setSociosFX6] = useState([]);
-  useEffect(() => {
-    // (async function () {})();
-    // (async () => {})();
-    (async () => {
-      let data = await api.getByCNPJ("61186888009301");
-      let socios_ = data.data.qsa;
-      setSocios(socios_);
-      //filtrar os socios que possui codigo_faixa_etaria igual a 7
-      setSociosFX7(socios_.filter((socio) => socio.codigo_faixa_etaria === 7));
+  let a = [1, 2, 3];
+  for (let i in a) {
+    console.log(i);
+  }
 
-      //filtrar os socios que possui codigo_faixa_etaria igual a 5
-      setSociosFX6(socios_.filter((socio) => socio.codigo_faixa_etaria === 5));
-    })();
-  }, []);
-  return (
-    <ScrollView style={{ flex: 1, padding: 20 }}>
-      <Text style={{ marginVertical: 20 }}>
-        TODOS OS SOCIOS COM FAIXA ETARIA 7 - QTY {sociosFX7.length}:
-      </Text>
-      {sociosFX7.map((socio) => {
-        return <Text>{socio.nome_socio}</Text>;
-      })}
-      <Text style={{ marginVertical: 20 }}>
-        TODOS OS SOCIOS COM FAIXA ETARIA 5 - QTY {sociosFX6.length}:
-      </Text>
-      {sociosFX6.map((socio) => (
-        <Text>{socio.nome_socio}</Text>
-      ))}
-    </ScrollView>
-  );
+  // if (estaPronto === 0) {
+  //   return <Text>Não pronto...</Text>;
+  // } else if (estaPronto === 1) {
+  //   return <Text>Carregando...</Text>;
+  // } else if (estaPronto === 2) {
+  //   return (
+  //     <ScrollView style={{ flex: 1, padding: 20 }}>
+  //       <Text style={{ marginVertical: 20 }}>
+  //         TODOS OS SOCIOS COM FAIXA ETARIA 7 - QTY {sociosFX7.length}:
+  //       </Text>
+  //       {sociosFX7.map((socio) => {
+  //         return <Text>{socio.nome_socio}</Text>;
+  //       })}
+  //       <Text style={{ marginVertical: 20 }}>
+  //         TODOS OS SOCIOS COM FAIXA ETARIA 5 - QTY {sociosFX6.length}:
+  //       </Text>
+  //       {sociosFX6.map((socio) => (
+  //         <Text>{socio.nome_socio}</Text>
+  //       ))}
+  //     </ScrollView>
+  //   );
+  // } else {
+  //   return <Text>Não encontrado</Text>;
+  // }
+
+  switch (estaPronto) {
+    case 0:
+      return <Text>Não pronto...</Text>;
+      break;
+    case 1:
+      return <Text>Carregando...</Text>;
+      break;
+    case 2:
+      return (
+        <ScrollView style={{ flex: 1, padding: 20 }}>
+          <Text style={{ marginVertical: 20 }}>
+            TODOS OS SOCIOS COM FAIXA ETARIA 7 - QTY {sociosFX7.length}:
+          </Text>
+          {sociosFX7.map((socio) => {
+            return <Text>{socio.nome_socio}</Text>;
+          })}
+          <Text style={{ marginVertical: 20 }}>
+            TODOS OS SOCIOS COM FAIXA ETARIA 5 - QTY {sociosFX6.length}:
+          </Text>
+          {sociosFX6.map((socio) => (
+            <Text>{socio.nome_socio}</Text>
+          ))}
+        </ScrollView>
+      );
+      break;
+    default:
+      return <Text>Não encontrado</Text>;
+      break;
+  }
 };
 
 export default Home;
